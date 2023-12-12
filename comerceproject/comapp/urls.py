8944c945d1a7624a59135c5a425d5c21cb8dfa9b
jijigomez14 <145670020+jijigomez14@users.noreply.github.com>
2023-12-12 13:18:32 +0530

    Add files via upload

---
+++ comerceproject/comapp/urls.py
@@ -0,0 +1,28 @@
+# from django.contrib import admin
+# from django.urls import path
+# from django.conf import settings
+# from django.conf.urls.static import static
+# from .views import hotel_image_view
+# from .views import success
+# from .views import display_hotel_images
+
+# urlpatterns = [
+# 	path('image_upload', hotel_image_view, name='image_upload'),
+# 	path('success', success, name='success'),
+# 	path('hotel_images', display_hotel_images, name = 'hotel_images'),
+# ]
+
+# if settings.DEBUG:
+# 	urlpatterns += static(settings.MEDIA_URL,
+# 						document_root=settings.MEDIA_ROOT)
+
+from django.urls import path
+from .views import index, header, footer,product_detail
+
+urlpatterns = [
+    path('', index, name='index'),
+    path('header/', header, name='header'),
+    path('footer/', footer, name='footer'),
+	path('product_detail/', product_detail, name='product_detail'),
+    # Add more URL patterns as needed
+]
---
+++ comerceproject/comapp/views.py
@@ -0,0 +1,53 @@
+from django.shortcuts import render
+
+# Create your views here.
+from django.shortcuts import render
+
+from django.http import HttpResponse
+# from django.shortcuts import render, redirect
+# from .forms import HotelForm
+# from .models import Hotel
+# # Create your views here.
+
+
+# def hotel_image_view(request):
+
+# 	if request.method == 'POST':
+# 		form = HotelForm(request.POST, request.FILES)
+
+# 		if form.is_valid():
+# 			form.save()
+# 			return redirect('success')
+# 	else:
+# 		form = HotelForm()
+# 	return render(request, 'hotel_image_form.html', {'form': form})
+
+
+# def success(request):
+# 	return render(request,'success.html')
+# # Python program to view
+# # for displaying images
+
+
+# def display_hotel_images(request):
+
+# 	if request.method == 'GET':
+
+# 		# getting all the objects of hotel.
+# 		Hotels = Hotel.objects.all()
+# 		return render(request, 'display_hotel_images.html', {'hotel_images': Hotels})
+from django.shortcuts import render
+
+def index(request):
+    # Any logic or data retrieval can be done here before rendering the template
+    return render(request, 'index.html')
+def header(request):
+    # Any logic or data retrieval for the about page can be done here
+    return render(request, 'header.html')
+def footer(request):
+    # Any logic or data retrieval for the about page can be done here
+    return render(request, 'footer.html')
+def product_detail(request):
+    # Replace this with your actual logic to retrieve product details
+    
+    return render(request, 'product.html')
